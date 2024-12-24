1ad59761598c2d18b5809baf80bd4c315b387314
OmarGoubail <omarsayed223@gmail.com>
2024-12-24 22:55:42 +0200

    feat: Switch to timestamps in some fields

--- src/types/task.ts
+++ src/types/task.ts
@@ -2,13 +2,12 @@
 export type TaskStatus = "active" | "completed" | "canceled";
 
 export interface Task {
-  id: string;
-  title: string;
-  notes?: string;
-  is_done: number | null; // Consider making this a boolean or a timestamp
-  created_at: number;
-  done_at: number | null;
-  journal_date: string; // Consider making this a Date or number (timestamp)
-  tags: string[];
-  priority: number;
+	id: string;
+	title: string;
+	notes?: string;
+	is_done: Date | null;
+	created_at: Date;
+	journal_date: Date;
+	tags: string[];
+	priority: number;
 }
